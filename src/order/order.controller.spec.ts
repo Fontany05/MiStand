f7c7a7891322e09323da2ff02615968785cbb0f8
anaf <any_822@hotmail.com>
2026-08-25 21:22:50 -0300

    fix: add mocks to order controller spec file

--- src/order/order.controller.spec.ts
+++ src/order/order.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+
+const mockOrderService = {
+  createOrder: jest.fn(),
+  getOrders: jest.fn(),
+  updateOrderStatus: jest.fn(),
+  deleteOrder: jest.fn(),
+};
 
 describe('OrderController', () => {
   let controller: OrderController;
@@ -7,9 +15,11 @@ describe('OrderController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
+      providers: [{ provide: OrderService, useValue: mockOrderService }],
     }).compile();
 
     controller = module.get<OrderController>(OrderController);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
